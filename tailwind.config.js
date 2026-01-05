1a87085d23c01d568475a5a665bedfa19b1b62a6
ioangheraszim <gijohny17@gmail.com>
2026-01-05 16:37:41 +0200

    feat: finished style in mobile, implemented border change on click
    - Finished styling on mobile
    - implemented JS for buttons to change border upon click

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(231, 69%, 60%)",
+        primaryDark: "hsl(229, 31%, 21%)",
+        accent: "hsl(0, 94%, 66%)",
+        background: "hsl(0, 0%, 97%)",
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+    },
+  },
+};
