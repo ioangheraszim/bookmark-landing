d45d9a6c58ae82b091fb1df83c326f22c0fbf535
ioangheraszim <gijohny17@gmail.com>
2026-01-06 12:59:25 +0200

    refactor: improve js logic, accesibility and semantic html across site
    - Updated all sections with proper semantic HTML elements and ARIA atributes
    - Implemented email validation logic
    - Implemented navigation opening/closing and transitions

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ import { data } from "./data.js";
 
 const buttons = document.querySelectorAll(".tab-btn");
 
+const emailInput = document.getElementById("email-input");
+const errorMsg = document.getElementById("error-msg");
+
+const btnOpen = document.getElementById("btn-open");
+const btnClose = document.getElementById("btn-close");
+const navList = document.getElementById("nav-list");
+
 function handleData(index) {
   const myData = data[index];
 
   const featureTitle = document.getElementById("feature-title");
   const featureSubtitle = document.getElementById("feature-subtitle");
-
-  // featureTitle.textContent = myData.title;
-  // featureSubtitle.textContent = myData.description;
 
   featureTitle.classList.add("opacity-0");
   featureSubtitle.classList.add("opacity-0");
@@ -46,4 +50,40 @@ document.querySelectorAll(".faq-btn").forEach((btn) => {
 
     arrowImg.classList.toggle("rotate-180", isClosed);
   });
+});
+
+btnOpen.addEventListener("click", () => {
+  navList.classList.add("translate-y-[0%]");
+  document.body.classList.add("overflow-hidden");
+});
+
+btnClose.addEventListener("click", () => {
+  navList.classList.remove("translate-y-[0%]");
+  document.body.classList.remove("overflow-hidden");
+});
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function showError() {
+  errorMsg.classList.remove("opacity-0", "-translate-y-6", "max-h-0");
+  errorMsg.classList.add("opacity-100", "translate-y-0", "max-h-20");
+  emailInput.classList.add("border-accent", "rounded-b-none", "border-2");
+}
+
+function hideError() {
+  errorMsg.classList.add("opacity-0", "-translate-y-6", "max-h-0");
+  errorMsg.classList.remove("opacity-100", "translate-y-0", "max-h-20");
+  emailInput.classList.remove("border-accent", "border-2");
+}
+
+emailInput.addEventListener("blur", () => {
+  const value = emailInput.value.trim();
+
+  if (value === "" || !isValidEmail(value)) {
+    showError();
+  } else {
+    hideError();
+  }
 });
